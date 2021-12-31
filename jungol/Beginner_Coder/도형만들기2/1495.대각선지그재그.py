e72b17187957d1d14a548e8e307632fae2fcebc0
이은성 <silvermaking@gmail.com>
2021-12-31 21:38:02 +0900

    도형만들기 edit

--- jungol/Beginner_Coder/도형만들기2/1495.대각선지그재그.py
+++ jungol/Beginner_Coder/도형만들기2/1495.대각선지그재그.py
@@ -4,16 +4,20 @@ number = 1
 i = j = 0
 arr[i][j] = number
 while True:
+    if (i, j) == (n-1, n-1):
+        break
     # 1. 아래로 한번 이동 or 오른쪽으로 이동
     if i + 1 < n:
         i += 1
-    else:
+    elif j + 1 < n:
         j += 1
+    else:
+        break
     number += 1
     arr[i][j] = number
 
     # 2. 오른쪽 위 대각선으로 이동
-    while i > 0 or j < n-1:
+    while i > 0 and j < n-1:
         i -= 1
         j += 1
         number += 1
@@ -22,13 +26,15 @@ while True:
     # 3. 오른쪽으로 한번 이동 or 아래로 이동
     if j + 1 < n:
         j += 1
-    else:
+    elif i + 1 < n:
         i += 1
+    else:
+        break
     number += 1
     arr[i][j] = number
 
     # 4. 왼쪽 아래 대각선으로 이동
-    while j > 0 or i < n-1:
+    while j > 0 and i < n-1:
         i += 1
         j -= 1
         number += 1
